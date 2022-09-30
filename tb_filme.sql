2d6b33129b6da5c8490d9bcd8b04d65f3b4ed609
DiegoCamposRJ <diegocamposrj@gmail.com>
2022-09-29 21:27:15 -0300

    tb_filme

---
+++ tb_filme.sql
@@ -0,0 +1,14 @@
+INSERT INTO tb_filme
+(id_filme,nome_filme,id_diretor,id_produtora,genero,mes,ano,valor_producao) VALUES
+(4,'Vingadores Ultimato',4,4,'Acao/Aventura',4,2019,13.990.000),
+(5,'O Rei Leao',5,4,'Epico/Drama',7,2019,8.314.495),
+(6,'Homem Aranha Longe de Casa',6,2,'Acao/Aventura',7,2019,5.659.635),
+(7, 'Star Wars Episódio IX – A Ascensão Skywalker',7,4,'Aventura/Ficcao Cientifica',12,2019,5.370.720),
+(8, 'Luta por Justiça',8,5,'Drama',2,2020,252.007.510),
+(9,'1917',9,6,'Drama/Guerra',1,2020,1.926.593.475),
+(10,'Os 7 de Chicago',10,7,'Drama',9,2020,40.000.950),
+(11,'Soul',11,4,'Familia/Comedia',10,2020,500.000.000),
+(12,'Aaron Sorkin',12,4,'Animacao/Fantasia',3,2021,652.000.000),
+(13,'Meu Pai',13,3,'Drama'4,2021,900.000),
+(14,'Judas e o Messias Negro',14,5,'Drama',2,2021,Null),
+(15,'Nomadland',15,8,'Drama/Faroeste',4,2021,197.500.000);
